c632838b1ae4233188455b4ec783c12458bc78bd
kvn098 <76583628+kvn098@users.noreply.github.com>
2020-12-29 18:54:46 +0530

    Add files via upload

---
+++ Pra7.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Console
+Module Module4
+    Sub Main()
+        Dim Month As Integer
+        WriteLine("Enter No")
+
+        Month = CInt(ReadLine())
+        Select Case Month
+            Case 1
+                WriteLine("January")
+            Case 2
+                WriteLine("February")
+            Case 3
+                WriteLine("March")
+            Case 4
+                WriteLine("April")
+            Case 5
+                WriteLine("May")
+            Case 6
+                WriteLine("June")
+            Case 7
+                WriteLine("July")
+            Case 8
+                WriteLine("August")
+            Case 9
+                WriteLine("September")
+            Case 10
+                WriteLine("October")
+            Case 11
+                WriteLine("November")
+            Case 12
+                WriteLine("December")
+            Case Else
+                WriteLine("Wrong Input!!")
+        End Select
+        Read()
+    End Sub
+End Module
